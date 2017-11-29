e6183cb8387f94dde90e01762ccb09334d9d58fd
Oladeji Famakinwa <ojf27@cam.ac.uk>
2017-11-29 12:14:06 +0000

    updated npm package info

--- publish2npm.sh
+++ publish2npm.sh
@@ -4,14 +4,18 @@ if [ -e '/tmp/release.npm' ]; then
 fi
 
 mkdir -p /tmp/release.npm/js
+mkdir -p /tmp/release.npm/css
 
 cp package.json /tmp/release.npm
 cp README.md /tmp/release.npm
 cp js/custom.js /tmp/release.npm/js/primo-explore-cambridge.js
+cp css/custom1.css /tmp/release.npm/css
 
 cd /tmp/release.npm
 
+npm pack
+
 #Enable commands below if you wish to publish to Npm
-#npm publish
-#cd -
+npm publish
+cd -
 #rm -Rf /tmp/release.npm
